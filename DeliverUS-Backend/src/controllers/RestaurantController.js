b75c063daaf06b530f5adf27ee2d6417791e5e40
GonzaloGomez29 <gongomcab@alum.us.es>
2025-03-20 11:43:50 +0100

    first commit

---
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -0,0 +1,126 @@
+import { Restaurant, Product, RestaurantCategory, ProductCategory } from '../models/models.js'
+
+const index = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        include:
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      },
+        order: [[{ model: RestaurantCategory, as: 'restaurantCategory' }, 'name', 'ASC']]
+      }
+    )
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const indexOwner = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        where: { userId: req.user.id },
+        include: [{
+          model: RestaurantCategory,
+          as: 'restaurantCategory'
+        }],
+        order: [['pinnedAt', 'DESC']]
+      })
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const create = async function (req, res) {
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = req.user.id
+  try {
+    if (req.body.pinned) {
+      newRestaurant.pinnedAt = new Date()
+    }
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const show = async function (req, res) {
+  // Only returns PUBLIC information of restaurants
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const update = async function (req, res) {
+  try {
+    await Restaurant.update(req.body, { where: { id: req.params.restaurantId } })
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const destroy = async function (req, res) {
+  try {
+    const result = await Restaurant.destroy({ where: { id: req.params.restaurantId } })
+    let message = ''
+    if (result === 1) {
+      message = 'Sucessfuly deleted restaurant id.' + req.params.restaurantId
+    } else {
+      message = 'Could not delete restaurant.'
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const togglePinned = async function (req, res) {
+  try {
+    const RestaurantToBeToggles = await Restaurant.findByPk(req.params.restaurantId)
+    if (RestaurantToBeToggles.pinnedAt) {
+      RestaurantToBeToggles.pinnedAt = null
+    } else {
+      RestaurantToBeToggles.pinnedAt = new Date()
+    }
+    await RestaurantToBeToggles.save()
+    res.json(RestaurantToBeToggles)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const RestaurantController = {
+  index,
+  indexOwner,
+  create,
+  show,
+  update,
+  destroy,
+  togglePinned
+}
+export default RestaurantController
---
+++ DeliverUS-Backend/src/middlewares/RestaurantMiddleware.js
@@ -0,0 +1,39 @@
+import { Restaurant, Order } from '../models/models.js'
+
+const checkRestaurantOwnership = async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    if (req.user.id === restaurant.userId) {
+      return next()
+    }
+    return res.status(403).send('Not enough privileges. This entity does not belong to you')
+  } catch (err) {
+    return res.status(500).send(err)
+  }
+}
+const restaurantHasNoOrders = async (req, res, next) => {
+  try {
+    const numberOfRestaurantOrders = await Order.count({
+      where: { restaurantId: req.params.restaurantId }
+    })
+    if (numberOfRestaurantOrders === 0) {
+      return next()
+    }
+    return res.status(409).send('Some orders belong to this restaurant.')
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+}
+const checkRestaurantIsPinned = async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    if (restaurant.pinned) {
+      return next()
+    }
+    return res.status(409).send('This restaurant is not pinned.')
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+}
+
+export { checkRestaurantOwnership, restaurantHasNoOrders, checkRestaurantIsPinned }
